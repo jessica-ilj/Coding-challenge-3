b021ddf921cc19857cffccb39f1017405c361193
Jessica Ilok <jilok@usf.edu>
2024-09-08 21:21:07 -0400

    Create determinePerformanceRating function

--- sales_performance_analyzer.js
+++ sales_performance_analyzer.js
@@ -5,4 +5,20 @@ function calculateAverageSales(salesfigures) {
     return (totalSales) / (salesfigures.length) ;
 }
 
+//Task 2- Create a Function to Determine Performance Rating
+function determinePerformanceRating(averageSales) {
+    if (averageSales > 10000) {        //Excellent for average sales above $10,000
+        return "Excellent";
+    } 
+    else if (averageSales >= 7000 && averageSales <= 10000) {  //Good for average sales between $7k and $10k
+        return "Good";
+    } 
+    else if (averageSales >= 4000 && averageSales < 7000) {  ///Satisfactory for average sales between $4k and $7k
+        return "Satisfactory";
+    } 
+    else {
+        return "Needs Improvement";  //Needs Improvement for average sales below $4,000
+    }
+};
+
 
